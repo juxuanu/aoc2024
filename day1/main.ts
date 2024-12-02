c8e30a38b7c1ed7f01addab7670e8d45422f2f1d
icar.nin_eposnow <icar.nin@eposnow.com>
2024-12-02 13:12:28 +0100

    day1 2

--- day1/main.ts
+++ day1/main.ts
@@ -1,21 +1,45 @@
 const input = Deno.readTextFileSync("day1/input.txt");
 
-const col1: number[] = [];
-const col2: number[] = [];
-let sum: number = 0;
+function part1() {
+  const col1: number[] = [];
+  const col2: number[] = [];
+  let sum: number = 0;
 
-input.split("\n").filter((line) => !!line.trim()).forEach((line) => {
-  console.info(line);
-  const [c1, c2] = line.split("   ");
-  col1.push(+c1);
-  col2.push(+c2);
-});
+  input.split("\n").filter((line) => !!line.trim()).forEach((line) => {
+    console.info(line);
+    const [c1, c2] = line.split("   ");
+    col1.push(+c1);
+    col2.push(+c2);
+  });
 
-col1.sort((a, b) => b - a);
-col2.sort((a, b) => b - a);
+  col1.sort((a, b) => b - a);
+  col2.sort((a, b) => b - a);
 
-for (let i = 0; i < col1.length; i++) {
-  sum += Math.abs(col1[i] - col2[i]);
+  for (let i = 0; i < col1.length; i++) {
+    sum += Math.abs(col1[i] - col2[i]);
+  }
+
+  console.log(sum);
 }
 
-console.log(sum);
+function part2() {
+  const col1: number[] = [];
+  const col2: Map<number, number> = new Map();
+  let similarity: number = 0;
+
+  input.split("\n").filter((line) => !!line.trim()).forEach((line) => {
+    console.info(line);
+    const [c1, c2] = line.split("   ");
+    col1.push(+c1);
+    col2.set(+c2, (col2.get(+c2) ?? 0) + 1);
+  });
+
+  for (const n of col1) {
+    similarity += n * (col2.get(n) ?? 0);
+  }
+
+  console.log(similarity);
+}
+
+part1();
+part2();
