fefdb8b97b20cbc5a451c9f95c0a8a07fa10cfe1
icar.nin_eposnow <icar.nin@eposnow.com>
2024-12-03 11:00:59 +0100

    separate day 3 part 1 & 2

--- day3/main.ts
+++ day3/main.ts
@@ -4,6 +4,23 @@ function part1() {
   let sum: number = 0;
 
   const regexResult = file.matchAll(
+    /mul\(([0-9]{0,3}),([0-9]{0,3})\)/g,
+  );
+
+  for (const item of regexResult) {
+    console.log(item[0], +item[1], +item[2]);
+    sum += +item[1] * +item[2];
+  }
+
+  console.log(sum);
+}
+
+part1();
+
+function part2() {
+  let sum: number = 0;
+
+  const regexResult = file.matchAll(
     /don't\(\)|do\(\)|mul\(([0-9]{0,3}),([0-9]{0,3})\)/g,
   );
 
@@ -24,4 +41,4 @@ function part1() {
   console.log(sum);
 }
 
-part1();
+part2();
