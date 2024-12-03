ab201a213a73b709d3af0e9aed0ba7e6df24c588
icar.nin_eposnow <icar.nin@eposnow.com>
2024-12-03 10:58:48 +0100

    day3 2

--- day3/main.ts
+++ day3/main.ts
@@ -4,12 +4,21 @@ function part1() {
   let sum: number = 0;
 
   const regexResult = file.matchAll(
-    /mul\(([0-9]{0,3}),([0-9]{0,3})\)/g,
+    /don't\(\)|do\(\)|mul\(([0-9]{0,3}),([0-9]{0,3})\)/g,
   );
 
+  let enabled = true;
   for (const item of regexResult) {
-    console.log(item[0], +item[1], +item[2]);
-    sum += +item[1] * +item[2];
+    const isMul = item[0][0] === "m";
+
+    const isDo = item[0] === "do()";
+    const isDont = item[0] === "don't()";
+
+    console.log(item[0], isMul, isDo, isDont);
+
+    if (isDo) enabled = true;
+    if (isDont) enabled = false;
+    if (isMul && enabled) sum += +item[1] * +item[2];
   }
 
   console.log(sum);
